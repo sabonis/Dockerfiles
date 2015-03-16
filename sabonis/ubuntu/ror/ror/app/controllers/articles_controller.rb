848ca1c931b674af5932ab97fe8f6ae327f4ebf9
sabonis <sabonis.tw@gmail.com>
2015-03-16 09:52:12 +0800

    Add U of CRUD.

--- sabonis/ubuntu/ror/ror/app/controllers/articles_controller.rb
+++ sabonis/ubuntu/ror/ror/app/controllers/articles_controller.rb
@@ -8,12 +8,29 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_param)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else 
+      render 'new'
+    end
+  end
+
+  def create
+    @article = Article.find(article_param)
+    if @article.update
+      redirect_to @article
+    else 
+      render 'edit'
+    end
   end
 
   private
